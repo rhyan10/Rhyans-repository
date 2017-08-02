8404c0365caeb0541c9f4fe0b6332a4643fed08a
rhyan10 <30473642+rhyan10@users.noreply.github.com>
2017-08-02 14:44:39 +0100

    Add files via upload
    My files

---
+++ Classes.py
@@ -0,0 +1,66 @@
+import scipy as sc
+import sympy as sp
+x = sp.Symbol('x')
+class functions:
+    def createfunctions(self,a,b,c,d,e):
+        function = None
+        i = 0
+        parameterlist = [a,b,c,d,e]
+        for i in parameterlist:
+            if  i == None:
+                pass
+            else:
+                if i == 0:
+                    function = a * x** 4
+                elif i == 1:
+                    function = function + b * x **3
+                elif i == 2:
+                    function = function + c * x **2
+                elif i ==3:
+                    function = function + d * x
+                else:
+                    function = function + e
+        #if (b == None and c == None and d == None and e == None):
+            #return a
+        #elif(c == None and d == None and e == None):
+            #return a * x + b
+        #elif(b == None and d ==None and e == None):
+            #return a* x**2 +
+        #elif(d == None and e == None):
+            #return a * x**2 + b * x + c
+        #elif(e == None):
+            #return a * x**3 + b * x**2 + c * x + d
+        #else:
+            #return a * x**4 + b * x**3 + c * x**2 + d * x + e
+class calculus():
+    def Integralindef(self,a,b,c,d,e):
+        if (b is None and c is None and d is None and e is None):
+            return("The integral of {} is {} x".format(a,a))
+        elif(c == None and d == None and e == None):
+            return("The integral of {} x plus {} is {} x squared plus {} x plus c".format(a,b,a/2,b))
+        elif(d == None and e == None):
+            return("The integral of {} x squared plus {} x plus {} is {} x cubed plus {} x squared plus {} x plus c ".format(a,b,c,a/3,b/2,c))
+        elif(e == None):
+            return("The integral of {} x cubed plus {} x squared plus {} x plus {} is {} x to the power of 4 plus {} x cubed plus {} x squared plus {} x + c ".format(a,b,c,d,a/4,b/3,c/2,d))
+        else:
+            return("The integral of {} x to the power of 4 plus {} x cubed plus {} x squared plus {} x plus {} is {} x to the power of 5 plus {} x to the power of 4 plus {} x cubed plus {} x squared plus {} x + c ".format(a,b,c,d,e,a/5,b/4,c/3,d/2,e))
+    def differential(self,a,b,c,d,e):
+        if (b == None and c == None and d == None and e == None):
+            return("The derivative of {} is 0".format(a))
+        elif(c == None and d == None and e == None):
+            return("The derivative of {} x plus {} is {}".format(a,b,a))
+        elif(d == None and e == None):
+            return("The derivative of {} x squared plus {} x plus {} is {} x plus {}  ".format(a,b,c,2*a    ,b))
+        elif(e == None):
+            return("The derivative of {} x cubed plus {} x squared plus {} x plus {} is {} x squared plus {} x plus {} ".format(a,b,c,d,3*a,2*b,c))
+        else:
+            return("The derivative of {} x to the power of 4 plus {} x cubed plus {} x squared plus {} x plus {} is {} x cubed plus {} x squared plus {} x plus {} ".format(a,b,c,d,e,4*a,3*b,2*c,d))
+    def areacurve(self,f,start,finish):
+        sc.inte
+class ApiConnect():
+    def twitterconnect():
+        Access_token = "892739265205063681-Og2W5jBCIZVKITrWILQhGc5ewiwHNFP"
+        Access_secret = "OdeTww1bslkbYjYaloeoZZQgpI1EVl6HoWpSkcxrYsNLx"
+        consumer_key = "61WsI5K9XqwxbD0qbMI585IzL"
+        consumer_secret = "FfxurEbL9jZGfVBhopuSTqpGVWiC7HAxWaxoIXmxo68aeKPR0Y"
+        
---
+++ maths.py
@@ -0,0 +1,97 @@
+import logging
+import os
+from flask import Flask
+import json
+from flask_ask import Ask, request, session, question, statement
+state = 0
+variable = None
+app = Flask(__name__)
+ask = Ask(app, "/")
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+from Classes import calculus
+from Classes import functions
+cal = calculus()
+func = functions()
+
+@ask.intent('IndefiniteIntIntegralIntent',convert={'a':int, 'b':int, 'c':int, 'd':int, 'e':int})
+def integrate(a,b,c,d,e):
+    speech_text = cal.Integralindef(a,b,c,d,e)
+    return question(speech_text)
+
+@ask.intent('firstderivative', convert={'a':int, 'b':int, 'c':int, 'd':int, 'e':int})
+def differentiate(a,b,c,d,e):
+    speech_text = cal.differential(a,b,c,d,e)
+    global state,variable
+    state = 1
+    variable = speech_text.split(" ")
+    return question(speech_text)
+@ask.intent('AreundercurveIntent',convert={'a':int, 'b':int, 'c':int, 'd':int, 'e':int, 'start':int, 'finish':int})
+def areacurve(a,b,c,d,e):
+    myfunction = func.createfunctions(a,b,c,d,e)
+    print (myfunction)  
+
+
+@ask.intent('SubtractionIntent', convert = {'a':int, 'b':int})
+def subtract(a,b):
+    c = a - b
+    return question("{} subtract {} equals {}".format(a,b,c))
+@ask.intent('MultiplyIntent', convert = {'a' :int, 'b':int })
+def multiplication(a,b):
+    c = a * b
+    return question("{} times by {} equals {}".format(a,b,c))
+@ask.intent('DivideIntent', convert = {'a':int, 'b':int})
+def division(a,b):
+     c = a/b
+     return question("{} divided by {} equals {}".format(a,b,c))
+@ask.intent('AddIntent', convert = {'a':int, 'b':int})
+def addition(a,b):
+     c = a+b
+     return question("{} divided by {} equals {}".format(a,b,c))
+
+@ask.intent('YesIntent')
+def yesintent():
+    global variable
+    if state == 1: # this will allow the user to differentiate the function as many times as they want
+        mylist = []
+        for listmember in variable:
+            if(listmember.isdigit()==True):
+                mylist.append(listmember)
+            else:
+                pass
+        length = len(mylist)
+        if length == 2 or length == 3:
+             speech_text = cal.differential(int(mylist[1]),None,None,None,None)
+             variable = speech_text.split(" ")
+             return question(speech_text)
+        elif length == 5:
+            speech_text = cal.differential(int(mylist[3]),int(mylist[4]),None,None,None)
+            variable = speech_text.split(" ")
+            return question(speech_text)
+        elif length == 7:
+            speech_text = cal.differential(int (mylist[4]), int(mylist[5]), int (mylist[6]),None,None)
+            variable = speech_text.split(" ")
+            return question(speech_text)
+        else:
+            speech_text = cal.differential(int(mylist[5]),int(mylist[6]),int(mylist[7]),int(mylist[8]),None)
+            variable = speech_text.split(" ")
+            return question(speech_text)
+@ask.intent('HelloWorldIntent', convert = {'a':int})
+def hello_word(a):
+       print session.attributes
+       print request
+       speech_text = "Hello there"
+       print a
+       return question(speech_text).simple_card("Hello word", speech_text)
+
+@ask.intent("AMAZON.NoIntent")
+def nointent():
+        speech_text = "sorry i didn't get what you said"
+        return question(speech_text)
+    # pass
+@ask.session_ended
+def session_ended():
+	return "{}", 200
+
+
+if __name__ == '__main__':
+	app.run(debug = True)
